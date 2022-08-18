873d3d1e85ec88c1648dd484aeba42554f471e94
drewrolla <andrewong007@gmail.com>
2022-08-17 21:46:21 -0400

    minor edit

--- howdy/src/views/Login.js
+++ howdy/src/views/Login.js
@@ -27,14 +27,14 @@ export default class Login extends Component {
     return (
         <form className='container col-4' onSubmit={(e)=>{this.sendLoginInfo(e)}}>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Username</label>
+                <label for="username" class="form-label">Username</label>
                 <input type="username" class="form-control" name="username" />
             </div>
 
 
             
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <label for="password" class="form-label">Password</label>
                 <input type="password" class="form-control" name='password' />
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
